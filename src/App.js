76179cb21b93fa69d6434f0d817871226cb6c86f
Aaron Agüero <aaron.aguero.estrella@gmail.com>
2021-03-29 23:24:50 -0300

    Se agrega menú de navegación como prueba

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+export default function App(){
+return (
+  <div className="App">
+    <h1>Menú de navegación</h1>
+    <nav className="navigationMenu">
+    <ul>
+      <li>
+        <a href="#" target="_blank">Home</a>
+      </li>
+      <li>
+        <a href="#" target="_blank">Products</a>
+      </li>
+      <li>
+        <a href="#" target="_blank">Blog</a>
+      </li>
+      <li>
+        <a href="#" target="_blank">Contacto</a>
+      </li>
+    </ul>
+    </nav>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  </div>
+);
 }
-
-export default App;
